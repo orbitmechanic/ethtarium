2c3558e4a06f538555997795330101cb5b192569
twistingzero <82689091+twistingzero@users.noreply.github.com>
2021-04-29 00:09:53 +0100

    catching up

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import {   Web3Provider } from "@ethersproject/providers"; //JsonRpcProvider,
+import { Web3Provider } from "@ethersproject/providers"; //JsonRpcProvider,
 
 //Structure
 //Pages
-import Home from "./components/Pages/Home";
+import Home from "./components/pages/Home";
 
 //Components
 import Map from "./components/map";
@@ -24,9 +24,9 @@ import Layout from "./components/Layout";
 import { getNode } from "./helpers/mapHelpers";
 
 // others
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-const INFURA_ID = 'd96fbcc2473445f091831576efa0255f';
+const INFURA_ID = "d96fbcc2473445f091831576efa0255f";
 // other chains?
 function App() {
   const [nodeSelected, setNodeSelected] = useState(null);
@@ -35,7 +35,6 @@ function App() {
   const [injectedProvider, setInjectedProvider] = useState();
   const [network, setNetwork] = useState();
   const [account, setAccount] = useState();
-
 
   const selectGraphEndpoint = (endpoint) => {
     setEndpoint(endpoint);
@@ -46,20 +45,18 @@ function App() {
     setNodeSelectedData(getNode(id));
   }, []);
 
-
-
-  async function donate(){
-    if(!injectedProvider || injectedProvider === 'not connected'){
+  async function donate() {
+    if (!injectedProvider || injectedProvider === "not connected") {
       await loadWeb3Modal();
     }
 
     // let balance = await provider.getBalance("") // see how to get address!
     // let balanceEth= ethers.utils.formatEther(balance)
     // let a_considerable_amount = float(balance) * 0.01 // balance is hex!
-    let signer = injectedProvider.getSigner()
+    let signer = injectedProvider.getSigner();
     let tx = signer.sendTransaction({
-        to: "0xdA839fc103363c8fAf3cF32052E204a70B2d5829", //our address
-        value: ethers.utils.parseEther("0.01")
+      to: "0xdA839fc103363c8fAf3cF32052E204a70B2d5829", //our address
+      value: ethers.utils.parseEther("0.01"),
     });
     // handle return and reject (error)
     // console.log(tx)
@@ -68,46 +65,48 @@ function App() {
   const loadWeb3Modal = useCallback(async () => {
     const web3Provider = await web3Modal.connect();
     // if(!web3Provider){ //nope.. rejection returns 4001.
-      // const provider = new ethers.providers.Web3Provider(window.ethereum)
+    // const provider = new ethers.providers.Web3Provider(window.ethereum)
     //   web3Provider = new JsonRpcProvider("https://mainnet.infura.io/v3/" + INFURA_ID)
     // }
-    if(web3Provider){
+    if (web3Provider) {
       let provider = new Web3Provider(web3Provider);
       setInjectedProvider(provider);
-      provider.getNetwork().then(data=>setNetwork(data.chainId));
-      provider.listAccounts().then(accounts=>{setAccount(accounts[0])})
+      provider.getNetwork().then((data) => setNetwork(data.chainId));
+      provider.listAccounts().then((accounts) => {
+        setAccount(accounts[0]);
+      });
       //other methods:
       // gasPrice = await provider.getGasPrice()
       // utils.formatUnits(gasPrice, "gwei") // to see it in gwei
-//https://docs.ethers.io/v5/api/providers/provider/#Provider--account-methods
-    }else{
-      setNetwork('not connected')
-      setAccount(null)
+      //https://docs.ethers.io/v5/api/providers/provider/#Provider--account-methods
+    } else {
+      setNetwork("not connected");
+      setAccount(null);
     }
     // const signer = web3Provider.getSigner() //use injectedProvider
   }, [setInjectedProvider]);
 
   useEffect(() => {
-    if (web3Modal.cachedProvider) { //isnt entering
+    if (web3Modal.cachedProvider) {
+      //isnt entering
       loadWeb3Modal();
     }
   }, [loadWeb3Modal]);
 
   return (
     <BrowserRouter>
-      <Layout >
+      <Layout>
         <Route exact path="/">
           <Landing selectGraphEndpoint={selectGraphEndpoint} />
-
         </Route>
         <Route path="/Map">
           <Map
             onNodeSelected={selectNode}
-            account = {account}
-            logoutOfWeb3Modal = {logoutOfWeb3Modal}
-            loadWeb3Modal = {loadWeb3Modal}
-            network = {network}
-             />
+            account={account}
+            logoutOfWeb3Modal={logoutOfWeb3Modal}
+            loadWeb3Modal={loadWeb3Modal}
+            network={network}
+          />
           <NodeOptions
             nodeSelected={nodeSelected}
             selectGraphEndpoint={selectGraphEndpoint}
@@ -125,7 +124,7 @@ function App() {
         <Route path="/Add">
           <AddNode />
         </Route>
-{/*
+        {/*
         <Route path="/Map">
           <Frontwip
             onNodeSelected={selectNode}
@@ -160,12 +159,11 @@ const logoutOfWeb3Modal = async () => {
   }, 1);
 };
 
- window.ethereum && window.ethereum.on('chainChanged', chainId => {
-  setTimeout(() => {
-    window.location.reload();
-  }, 1);
-})
-
-
+window.ethereum &&
+  window.ethereum.on("chainChanged", (chainId) => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  });
 
 export default App;
